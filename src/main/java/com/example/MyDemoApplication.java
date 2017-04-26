14cb853ad44b3922ca87e686973aa5920441fd14
Premanand C <premanandc@gmail.com>
2017-04-26 07:16:31 -0400

    Added checkstyle integration to project

--- src/main/java/com/example/MyDemoApplication.java
+++ src/main/java/com/example/MyDemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyDemoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(MyDemoApplication.class, args);
+  }
 }
